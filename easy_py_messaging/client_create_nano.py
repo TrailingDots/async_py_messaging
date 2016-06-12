386aeb8d6bb1a3adb401c5997e1dbf4eb392ebfe
Trailing Dots <trailingdots@gmail.com>
2016-06-12 11:47:07 -0700

    Starting async version of messages.

--- easy_py_messaging/client_create_nano.py
+++
@@ -1,9 +0,0 @@
-import client_create_class
-client = client_create_class.ClientCreateClass({})
-client.start()
-msg = 'Hello world!'
-print 'Sending : "%s"' % msg
-response = client.send(msg)
-print 'Response: "%s"\n' % response
-client.join()
-
--- easy_py_messaging/server_create_nano.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python
-import server_create_class
-def handle_request(ident, msg):
-    return ident, msg + '_resp'
-config = { 'in_fcn': handle_request }
-server = server_create_class.ServerCreateClass(config)
-server.start()
-while True:
-    server.join(1)
